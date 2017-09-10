facff3146849ca195a931686d26f64a047925b5d
Mike Bozee <msbozee@gmail.com>
2017-09-10 19:17:21 +0000

    client credentials flow works

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import sys
 
 from flask import Blueprint, Flask, render_template
 import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy.util as util
 
 app = Flask(__name__)
@@ -21,9 +22,22 @@ def index():
     ''' Main view. '''
     # sp = spotipy.Spotify()
 
-    # results = sp.search(q='weezer', limit=20)
-    # for i, t in enumerate(results['tracks']['items']):
-    #     print(' ', i, t['name'])
+    client_credentials_manager = SpotifyClientCredentials()
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+    # playlists = sp.user_playlists('spotify')
+    # while playlists:
+    #     for i, playlist in enumerate(playlists['items']):
+    #         print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
+    #     if playlists['next']:
+    #         playlists = sp.next(playlists)
+    #     else:
+    #         playlists = None
+
+    results = sp.search(q='helmet', limit=20)
+    for i, t in enumerate(results['tracks']['items']):
+        print(' ', i, t['name'])
+
     return render_template('index.html')
     
 
